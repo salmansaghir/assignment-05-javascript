6d02362f7b3f5fcd28fcac3259ae13fdb2fc24bb
salman saghir <salman.saghir1@gmail.com>
2024-03-28 00:20:52 +0500

    arry & loop

---
+++ app.js
@@ -0,0 +1,50 @@
+//-------ARRAY AND LOOP---------
+//Q1
+// var arr;
+// arr=[[]];
+//Q2
+// var arr;
+// arr=[[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+
+// // document.write(`${arr[0].join(" ")}<br>${arr[1].join(" ")}<br>${arr[2].join(" ")}`)
+// for(var i=0; i<=3; i++){
+//     document.write(arr[i].join(" ")+"<br>")
+// }
+//Q3
+// for(var i=1; i<=10; i++){
+//     document.write(i+"<br>")
+// }
+//Q4
+// var table=+prompt("Enter a number of table")
+// for(var i=1; i<=10; i++){
+//     document.write(`${table} * ${i} = ${table*i} <br>`)
+// }
+//Q4.b
+// var table=+prompt("Enter the number of table")
+// var rang=+prompt("Rang of the table ")
+// for(var i=1; i<=rang; i++){
+//     document.write(`${table} * ${i} = ${table*i} <br>`)
+// }
+//Q5
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"]
+// document.write(fruits +"<br>")
+// for(var i=0; i<=4; i++)
+// document.write(`<br> The Element at Index ${i} ${fruits[i]}`)
+//Q6
+// document.write("Counting:<br>")
+// for(var i=1; i<=15; i++){
+//     document.write(i,",")
+// }
+// document.write("<br>Reverse Counting:<br>")
+// for(var i=10; i>=1; i--){
+//     document.write(i,",")
+// }
+// document.write("<br>Even:<br>")
+// for(var i=1; i<=20; i+=i){
+//     document.write(i,",")
+// }
+
+// document.write("<br>Odd:<br>")
+// for(var i=1; i<=20; i+=2){
+//     document.write(i,",")
+// }
